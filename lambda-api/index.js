9d486ddd2339c71e5b972544759d72aec7603d61
motionbit95 <motionbit.dev@gmail.com>
2024-11-04 11:43:18 +0900

    Swagger 문서 수정

--- lambda-api/index.js
+++ lambda-api/index.js
@@ -13,12 +13,44 @@ const swaggerOptions = {
     info: {
       title: "Radisen AI License API",
       version: "1.0.0",
+      description: `
+### 테스트용 JWT 토큰
+아래의 JWT 토큰을 사용하여 테스트할 수 있습니다:
+
+- **토큰**: \`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiUmFkaXNlbiIsImlhdCI6MTczMDY4NzIzNSwiZXhwIjoyMDQ2MjYzMjM1fQ.XDnVAE3wV_5RsUhr6QjpvJzGI0nCCxNG4gZRYhj3xp4\`
+
+- **유효기간**: 10년
+- **사용자 ID**: \`Radisen\` (고유한 테스트용 ID)
+
+⚠️ **주의**: 이 토큰은 실제 환경에서는 사용하지 마십시오.
+
+      `,
     },
   },
   apis: ["./companyRouter.js", "./licenseRouter.js"],
 };
+
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Swagger UI 초기화 시 requestInterceptor 설정 추가
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocs, {
+    swaggerOptions: {
+      requestInterceptor: (req) => {
+        if (
+          req.headers.Authorization &&
+          !req.headers.Authorization.startsWith("Bearer ")
+        ) {
+          req.headers.Authorization = `Bearer ${req.headers.Authorization}`;
+        }
+        return req;
+      },
+    },
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
